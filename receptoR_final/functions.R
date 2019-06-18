4cc5e7d441996d9dfb857d3f1866ab67bda633fe
Derek <derek.toms@ucalgary.ca>
2019-06-18 10:22:49 -0600

    Update functions.R
    Maybe it's fixed now...

--- receptoR_final/functions.R
+++ receptoR_final/functions.R
@@ -24,10 +24,10 @@ desat = function(cols, sat=0.5) {
 processData = function(finished_table,datasetID,userComments,gpl,userDB){
     ## categories need to have R-safe names so we'll store the user input as a new column
     finished_table$category.labels <- finished_table$category
-    safeNames <- make.names(levels(finished_table$category),unique=TRUE)
-    finished_table$category <- factor(finished_table$category, levels = levels(finished_table$category), labels=safeNames)
+    safeNames <- make.names(levels(factor(finished_table$category)),unique=TRUE)
+    finished_table$category <- factor(finished_table$category, levels = levels(factor(finished_table$category)), labels=safeNames)
     ## colours
-    catCol <- factor(brewer.pal(9,"Set1")[1:length(levels(finished_table$category))])
+    catCol <- factor(brewer.pal(9,"Set1")[1:length(levels(factor(finished_table$category)))])
     finished_table$colours <- finished_table$category
     finished_table$colours <- factor(finished_table$colours, levels=levels(finished_table$category), labels=catCol)
     
